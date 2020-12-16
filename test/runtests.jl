3397a28952b12be611ce3e1e7abfc67d876c23e7
hyrodium <hyrodium@gmail.com>
2020-12-17 01:22:29 +0900

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using ExportNURBS
 using Test
+using Random
+using BasicBSpline
+using Colors
 
 @testset "ExportNURBS.jl" begin
-    @test true
+    Random.seed!(42)
+
+    p = 2 # degree of polynomial
+    k = Knots(1:8) # knot vector
+    P = FastBSplineSpace(p, k) # B-spline space
+    rand_a = [rand(2) for i in 1:dim(P), j in 1:dim(P)]
+    a = [[2 * i - 6.5, 2 * j - 6.5] for i in 1:dim(P), j in 1:dim(P)] + rand_a # random generated control points
+    M = BSplineSurface([P, P], a) # Define B-spline manifold
+
+    @testset "svg" begin
+        save_svg("test.svg",M)
+        @test isfile("test.svg")
+    end
+    @testset "png" begin
+        save_png("test.png",M)
+        @test isfile("test.png")
+    end
+    @testset "color-png" begin
+        color(u) = rand(RGB)
+        save_png("test_color.png", M, color)
+        @test isfile("test_color.png")
+    end
 end
